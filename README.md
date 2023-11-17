1dcf8c9752f6780cd6a3182fcfef77daf86553bd
Ebenclaw <rebekasanper15@gmail.com>
2023-11-17 08:38:35 +0100

    Version1.0

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proyecto DWES
+## Autor
+Rebeca Sánchez Pérez
+## Requisitos minimos del desarrollo
+- [Apache2.4.52]
+- [MySQL8.0.35]
+- [PHP8.1]
+- [XDebug3.1.2]
+## Descripcion
+Documentacion de la asignatura de desarrollo web entorno servidor: Index, tema1 y tema2
+## Funcionalidades
+## Entorno de desarrollo
+[http://daw204.isauces.local/204DWESProyectoDWES/indexProyectoDWES.php](http://daw204.isauces.local/204DWESProyectoDWES/indexProyectoDWES.php)
+## Entorno de explotacion
---
+++ indexProyectoDWES.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="webroot/css/style.css">
+    <link rel="icon" type="image/x-icon" href="../webroot/image/flora.png">
+    <title>Rebeca Sánchez Pérez</title>
+</head>
+
+<body>
+    <header>
+        <a href="../index.html"><img id="logo" src="webroot/image/logo.png" alt="Logo"></a>
+        <h1>Desarrollo Web en entorno Servidor - DWES</h1>
+    </header>
+    <main>
+        <ul>
+            <li><a href="codigoPHP/indexTema1.php">Tema 1 - DESARROLLO WEB EN ENTORNO SERVIDOR</a></li>
+            <li><a href="codigoPHP/indexTema2.php">Tema 2 - INSTALACIÓN, CONFIGURACIÓN Y DOCUMENTACIÓN DEL ENTORNO DE DESARROLLO Y DEL ENTORNO DE EXPLOTACIÓN</a></li>
+            <li><a href="../204DWESProyectoTema3/indexProyectoTema3.php">Tema 3 - CARACTERÍSTICAS DEL LENGUAJE PHP </a></li>
+            <li><a href="../204DWESProyectoTema4/indexProyectoTema4.php">Tema 4 - TÉCNICAS DE ACCESO A DATOS EN PHP</a></li>
+            <li><a href="">Tema 5 - DESARROLLO DE APLICACIONES WEB UTILIZANDO CÓDIGO EMBEBIDO</a></li>
+            <li><a href="">Tema 6 - PROGRAMACIÓN ORIENTADA A OBJETOS EN PHP</a></li>
+            <li><a href="">Tema 7 - PROGRAMACIÓN DE SERVICIOS WEB</a></li>
+            <li><a href="">Tema 8 - DESARROLLO DE APLICACIONES WEB HÍBRIDAS</a></li>
+        </ul>
+    </main>
+    <footer>
+        <div id="derechos">2023-2024 © Todos los derechos reservados: <a href="../index.html">Rebeca Sánchez Pérez</a></div>
+        <div id="fotos">
+            <a href="https://github.com/Ebenclaw" target="_blank"><img id="git" src="webroot/image/GitHub.png" alt="GitHub"></a>
+            <a href="http://ieslossauces.centros.educa.jcyl.es/sitio/" target="_blank"><img id="sauces" src="webroot/image/sauces.png" alt="Sauces"></a>
+            <a href="../index.html"><img id="home" src="webroot/image/home.png" alt="Inicio"></a>
+        </div>
+    </footer>
+</body>
+
+</html>
